0feb25de51ab63e214d13f0573f04aa417178829
spacekay <bethjkim0311@gmail.com>
2023-06-24 02:17:57 +0900

    fix: No argument constructor 추가

--- src/main/java/kr/megaptera/backendsurvivalweek10/models/CartId.java
+++ src/main/java/kr/megaptera/backendsurvivalweek10/models/CartId.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Embeddable;
 
 @Embeddable
 public class CartId extends EntityId {
-    // TODO: 계정 별로 카트를 생성할 수 있도록 수정해 주세요.
+
+    private CartId() {
+        super();
+    }
+
     public CartId(String value) {
         super(value);
     }
